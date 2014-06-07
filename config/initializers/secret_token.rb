a07c14bf9d006692455a169aec3e9e0a23908d78
jampou <jampou@gmail.com>
2014-06-07 09:16:58 +0800

    init 0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JTwitter::Application.config.secret_token = '546ed7ae3c8b3690e1139c5d6fc33ecc4c0e180d0c35a16081edbe6e2ea2f4cfa9bfc9a2f26e63c8edcbf4aa09e3dc59482ae9774d2744fec4f1b02b79f52939'
